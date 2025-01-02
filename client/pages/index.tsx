99b3b78e02a852f8a818202e87328cbdb0a47799
chmahesh <chmahesh997@gmail.com>
2025-01-02 22:21:04 +0530

    added sidebarmenu

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,114 +1,68 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+interface sidebarButtons {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sideBarMenuitems: sidebarButtons[] = [
+  {
+    title: "Home",
+    icon: <FaXTwitter />,
+  },
+  {
+    title: "Explore ",
+    icon: <FaXTwitter />,
+  },
+  {
+    title: "Notification",
+    icon: <FaXTwitter />,
+  },
+  {
+    title: "Messages",
+    icon: <FaXTwitter />,
+  },
+  {
+    title: "Communities",
+    icon: <FaXTwitter />,
+  },
+  {
+    title: "Profile",
+    icon: <FaXTwitter />,
+  },
+];
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={inter.className}>
+      <div className=" grid grid-cols-12 h-screen w-screen ">
+        <div className=" col-span-3 border-r-[0.5px] border-r-slate-200 flex">
+          <div className=" px-24 pt-1">
+            <div className=" text-[30px] rounded-full w-fit p-[10px] hover:bg-gray-50 dark:hover:bg-gray-900  transition-all cursor-pointer">
+              <FaXTwitter />
+            </div>
+            <div>
+              <ul className=" mt-2 text-[19px] font-medium">
+                {sideBarMenuitems.map((item) => (
+                  <li className=" flex justify-start  items-center gap-3 px-4 py-3 mt-[6px] pr-2 rounded-full hover:bg-gray-50 dark:hover:bg-gray-900  transition-all cursor-pointer">
+                    <span>{item.icon}</span>
+                    <span>{item.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <button className=" mt-4 px-[90px] py-[14px] w-fit bg-white text-black rounded-full font-bold">
+                Post
+              </button>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className=" col-span-5 border-r-[0.5px] border-r-slate-200"></div>
+        <div className=" col-span-4"></div>
+      </div>
     </div>
   );
 }
